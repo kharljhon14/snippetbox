d8c7c0621f4e64aed95db7fa26b39f1ba81bafcd
Kharl <enriquezkharl14@gmail.com>
2024-10-28 15:48:20 +0800

    [Feat] Added psql connection and driver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
-	"strings"
+
+	_ "github.com/lib/pq"
 )
 
 type application struct {
@@ -13,44 +15,28 @@ type application struct {
 	infoLog  *log.Logger
 }
 
-func neuter(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.HasPrefix(r.URL.Path, "/") {
-			http.NotFound(w, r)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	defaultAddr := os.Getenv("ADDR")
 
 	addr := flag.String("addr", defaultAddr, "HTTP network address")
+	dsn := flag.String("dsn",
+		"host=localhost user=web password=pass dbname=snippetbox sslmode=disable",
+		"PostgreSQL data source name")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	app := &application{errorLog: errorLog, infoLog: infoLog}
-
-	// Router
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// All URL paths that starts with "/static/"
-	// Strip "/static" prefix before the request reachers the file server
-	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
-
-	f, err := os.OpenFile("./tmp/info.log", os.O_RDWR|os.O_CREATE, 0666)
+	db, err := openDb(*dsn)
 
 	if err != nil {
-		log.Fatal(err)
+		errorLog.Fatal(err)
 	}
 
-	defer f.Close()
+	defer db.Close()
+
+	app := &application{errorLog: errorLog, infoLog: infoLog}
 
 	srv := &http.Server{
 		Addr:     *addr,
@@ -66,4 +52,18 @@ func main() {
 	err = srv.ListenAndServe()
 
 	errorLog.Fatal(err)
+}
+
+func openDb(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,12 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
+
+func neuter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasPrefix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
 
 func (app *application) routes() *http.ServeMux {
+	// Router
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	// All URL paths that starts with "/static/"
+	// Strip "/static" prefix before the request reachers the file server
+	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
 
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
