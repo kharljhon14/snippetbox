78a3a845642c2e83e5f41b322d43b6179391e5dc
Kharl <enriquezkharl14@gmail.com>
2024-11-17 16:07:36 +0800

    [Test] Health test

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -297,3 +297,7 @@ func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
 
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Ok"))
+}
